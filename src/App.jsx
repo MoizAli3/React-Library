d15feb8b9291c5b68eef5e65ee67d74e5e4f0c4b
Moiz Ali <102537980+MoizAli3@users.noreply.github.com>
2025-04-04 11:59:15 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header"
+import Notification from "./components/Notification"
+import Drawer from "./components/Drawer"
+
+function App() {
+
+
+  return (
+    <>
+    <Header /> <br /> <br />
+    <Notification/> <br /> <br />
+    <Drawer />
+
+    </>
+  )
+}
+
+export default App
